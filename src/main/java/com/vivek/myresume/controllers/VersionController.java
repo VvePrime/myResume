6440b4ffa41e0c70b562e8a6399c1e0cd30cab20
VvePrime <vive021172@gmail.com>
2026-03-01 11:55:06 +0530

    Dockerfile, version endpoint, version in env vars

---
+++ src/main/java/com/vivek/myresume/controllers/VersionController.java
@@ -0,0 +1,19 @@
+package com.vivek.myresume.controllers;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/v1")
+public class VersionController {
+
+    @Value("${app.version}")
+    private String version;
+
+    @GetMapping("/version")
+    public String version() {
+        return version;
+    }
+}
